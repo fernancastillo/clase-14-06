9f05dc7fecb849964ac83f9df453639b3eb71d17
fernandoCastillo <fern.castillop@duocuc.cl>
2024-06-14 11:05:25 -0400

    ejercicio solo con menú

---
+++ trabajo.py
@@ -0,0 +1,20 @@
+import os, time
+
+while True:
+    print("MENÚ TRABAJADORES")
+    print("1. Registrar trabajador")
+    print("2. Listar todos los trabajadores")
+    print("3. Imprimir planilla de suldos")
+    print("4. Salir del programa")
+    opc=int(input("Ingrese opción: "))
+    os.system("cls")
+    if opc==1:
+        pass
+    elif opc==1:
+        pass
+    elif opc==2:
+        pass
+    else:
+        print("Gracias por usar el programa, adios!")
+        break
+    time.sleep(3)
